48130f2884cc8b8d4d10ddfeb509c44f97f2060e
Brian Park <brian.parkjy@gmail.com>
2022-06-12 23:43:57 -0700

    [UPDATE] update to add,edit,and delete activity. also working on getting time duration for each category

--- ver_cli/database.py
+++ ver_cli/database.py
@@ -1,6 +1,8 @@
 import sqlite3
+from datetime import datetime, date, timedelta
 
-conn = sqlite3.connect('activity.db')
+conn = sqlite3.connect('activity.db', detect_types=sqlite3.PARSE_DECLTYPES |
+                             sqlite3.PARSE_COLNAMES)
 c = conn.cursor()
 
 def closeConn():
@@ -9,8 +11,8 @@ def closeConn():
 
 def createTable():
     c.execute('''CREATE TABLE activity (
-        start_time text,
-        end_time text,
+        start_time TIMESTAMP,
+        end_time TIMESTAMP,
         category text,
         note text
     )''')
@@ -23,15 +25,46 @@ def dropTable():
 
 def addActivity(start, end, category, note):
     c = conn.cursor()
+    start = datetime(date.today().year,date.today().month,date.today().day,int(start[:start.index(":")]),int(start[start.index(":")+1:]))
+    end = datetime(date.today().year,date.today().month,date.today().day,int(end[:end.index(":")]),int(end[end.index(":")+1:]))
     c.execute("INSERT INTO activity (start_time, end_time, category, note) VALUES (?,?,?,?)", (start, end, category, note))
     conn.commit()
+    addDuration(start, end, category)
 
-def editActivity():
-    pass
+def editActivity(edit_idx, start, end, category, note):
+    c = conn.cursor()
+    start = datetime(date.today().year,date.today().month,date.today().day,int(start[:start.index(":")]),int(start[start.index(":")+1:]))
+    end = datetime(date.today().year,date.today().month,date.today().day,int(end[:end.index(":")]),int(end[end.index(":")+1:]))
+
+    '''
+    fetch original row
+        if category has changed:
+            subtract duration from old category
+            add duration to new category
+        else (category is same)
+            if new end - start == old end - start:
+                update values
+            else (duration has changed):
+                subtract old duration from category
+                add new duration to category
+    '''
+
+    c.execute("UPDATE activity SET start_time=?, end_time=?, category=?, note=? WHERE rowid=?", (start, end, category, note, edit_idx))
+    conn.commit()
+    print("Edited activity")
 
 def deleteActivity(del_idx):
+    # c.execute("SELECT * FROM activity WHERE rowid=?", (del_idx))
+    # time = c.fetchall()
+
+    
     c.execute("DELETE FROM activity WHERE rowid=?", (del_idx,))
     conn.commit()
+    ''' 
+    fetch row to be deleted
+        subtract duration from duration table with same category
+    '''
+
     print("Deleted activity")
 
 def getCount():
@@ -44,10 +77,69 @@ def fetchActivity():
     c.execute('''SELECT rowid, * FROM activity ''')
     items = c.fetchall()
     for log in items:
-        print(str(log[0]) + '\t ' + log[1] + '\t ' + log[2] + '\t ' + log[3] + '\t ' + log[4])
-        #print(log)
+        print(log[0], '\t ', log[1], '\t ', log[2], '\t ', log[3], '\t ', log[4])
 
-def calcDuration():
+def fetchOne(idx):
+    c = conn.cursor()
+    c.execute(''' SELECT * FROM activity WHERE rowid=?''', (idx,))
+    items = c.fetchone()
+    return items
+
+def createDuration():
+
+    c.execute('''CREATE TABLE time (
+    category text,
+    duration text
+    )''')
+    conn.commit()
+
+def dropDuration():
+    c.execute('''DROP TABLE time''')
+    conn.commit()
+
+def addDuration(start, end, category):
+    c = conn.cursor()
+    c.execute('''SELECT DISTINCT(category) from time''')
+    items = [i[0] for i in c.fetchall()]
+    time = end - start
+    
+    if category in items:
+        print("category already exists\n")
+        c.execute('''SELECT * from time WHERE category=?''',(category,))
+        item = c.fetchall()
+
+        print(item)
+        print(item[0][1])
+        print(type(item[0][1]))
+
+        t = datetime.strptime(item[0][1], "%H:%M:%S")
+        td2 = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+
+        print("****************")
+        print(t)
+        print(type(t))
+
+        print(td2)
+        print(type(td2))
+        print("*****************")
+
+        time += td2
+        c.execute('''UPDATE time SET duration=? WHERE category=?''', (str(time), category))
+
+    else:
+        # add new row with category, duration
+        c.execute('''INSERT INTO time (category, duration) VALUES (?,?)''', (category, str(time)))
+        print('Duration inserted sucessfully. ')
+    
+    conn.commit()
+
+def editDuration():
+    # TO_DO
     pass
+
+def subtractDuration(start, end, category):
+    # TO_DO
+    pass
+
 
 
--- ver_cli/timeLog.py
+++ ver_cli/timeLog.py
@@ -3,8 +3,15 @@ import database
 
 if __name__ == "__main__":
     userInput = ''
+    duration = {}
 
-    # mock data
+    # database.dropTable()
+    # database.dropDuration()
+    # database.createDuration()
+    # database.dropDuration()
+    # database.createDuration()
+
+    # # mock data
     # database.addActivity('12:00','13:30',"food","this is a note")
     # database.addActivity('13:31','14:00',"study","this is another note")
     # database.addActivity('14:00','15:00',"workout","this is another random note")
@@ -22,14 +29,28 @@ if __name__ == "__main__":
             \n(5) Print time usage summary\n(q) Quit the program\n")
 
             if userInput == '1': # Add a log entry
-                startTime = input("Please enter the start time of this activity")
-                endTime = input("Please enter the end time of this activity")
-                category = input("Please enter the category for this activity")
-                note = input("Please wrtie down a note for this activity")
+                startTime = input("Please enter the start time of this activity ")
+                endTime = input("Please enter the end time of this activity ")
+                category = input("Please enter the category for this activity ")
+                note = input("Please wrtie down a note for this activity ")
                 database.addActivity(startTime, endTime, category, note)
                 
             elif userInput == '2': # Edit existing log entry
-                pass
+                database.fetchActivity()
+                edit_idx = int(input("Please enter the activity id # you wish to edit: "))
+                while not 0 < edit_idx <= database.getCount():
+                    edit_idx = int(input("Invalid range: Please enter the activity id # you wish to delete: "))
+                items = list(database.fetchOne(edit_idx))
+                print(f"Start: {items[0]}\nEnd: {items[1]}\nCategory: {items[2]}\nNote: {items[3]}\n")
+                user_input = input(("Please select the item you would like to edit\n Start: 0\tEnd: 1\tCategory: 2\t Note: 3\nEnter 'q' to cancel "))
+                while user_input != 'q':
+                    if 0 <= int(user_input) <= 3:
+                        change = input("Please enter the change: ")
+                        items[int(user_input)] = change
+                    print("Please select the item you would like to edit\n Start: 0\tEnd: 1\tCategory: 2\t Note: 3 ")
+                    user_input = input("Enter q to save and exit ")
+                database.editActivity(edit_idx, items[0], items[1], items[2], items[3])
+
             elif userInput == '3': # Delete log entry
                 database.fetchActivity()
                 del_item = int(input("Please enter the activity id # you wish to delete: "))
